a0f03e5ef91e2ff85f64326e3cd0727396eeb6ca
Ife Onuorah <ifeonuorah@gmail.com>
2024-10-26 02:21:53 -0300

    Style main video basic structure

--- src/components/CurrentVideo/CurrentVideo.jsx
+++ src/components/CurrentVideo/CurrentVideo.jsx
@@ -2,12 +2,14 @@ import "./CurrentVideo.scss";
 
 function CurrentVideo({ video }) {
 	return (
-		<video
-			controls
-			src={video.video}
-			poster={video.image}
-			className="currentVideo__video"
-		></video>
+		<div className="currentVideo-container">
+			<video
+				controls
+				src={video.video}
+				poster={video.image}
+				className="currentVideo__video"
+			></video>
+		</div>
 	);
 }
 
