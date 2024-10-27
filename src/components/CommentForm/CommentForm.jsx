39d2decf36101c572be7adb4f91c40b4e0a93695
Ife Onuorah <ifeonuorah@gmail.com>
2024-10-27 00:55:55 -0300

    Add styling for hover functionality

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,6 +1,9 @@
 import "./CommentForm.scss";
 
 function CommentForm({ currentVideo, avatar }) {
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
 	return (
 		<div className="comment__input">
 			<span className="comment__number">
@@ -8,13 +11,17 @@ function CommentForm({ currentVideo, avatar }) {
 			</span>
 			<div className="comment__form-container">
 				<img src={avatar} className="comment__avatar" />
-				<form className="comment__form">
+				<form
+					className="comment__form"
+					onSubmit={(event) => handleSubmit(event)}
+				>
 					<legend className="comment__form-title">Join the conversation</legend>
 					<div className="comment__form-fields">
 						<textarea
 							name="comment__text"
 							placeholder="Add a new comment"
 							className="comment__text"
+							required
 						></textarea>
 						<button className="comment__button">comment</button>
 					</div>
